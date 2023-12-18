72c7027b76659cb516a3959435f1932c4c2e8feb
mousamax <mousa.abdelmaksoud@tum.de>
2023-12-18 23:32:40 +0100

    feat: Slf4j for logging

--- backend/project-service/src/main/java/com/onlineide/projectservice/service/ProjectService.java
+++ backend/project-service/src/main/java/com/onlineide/projectservice/service/ProjectService.java
@@ -4,6 +4,7 @@ import com.onlineide.projectservice.dto.ProjectRequest;
 import com.onlineide.projectservice.dto.ProjectResponse;
 import com.onlineide.projectservice.model.Project;
 import com.onlineide.projectservice.repository.ProjectRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,6 +12,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.util.List;
 
 @Service
+@Slf4j
 public class ProjectService {
 
     @Autowired
@@ -33,6 +35,7 @@ public class ProjectService {
 
             List<Project> projects = projectRepository.findAll();
 
+            log.info("projects: {}", projects);
             return ProjectResponse.fromProjects(projects);
     }
 }
