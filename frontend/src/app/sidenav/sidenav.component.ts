1f6cf94bcf176a2c8135355801e7872534290b50
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2023-12-20 17:14:39 +0100

    add file list

--- frontend/src/app/sidenav/sidenav.component.ts
+++ frontend/src/app/sidenav/sidenav.component.ts
@@ -4,18 +4,54 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { CodeEditorComponent } from '../code-editor/code-editor.component';
 import { ApiClientService } from '../api-client.service';
 import { TerminalComponent } from '../terminal/terminal.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { File } from '../interfaces/file';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css'],
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, CodeEditorComponent, TerminalComponent],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    CodeEditorComponent,
+    TerminalComponent,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+  ],
 })
 export class SidenavComponent {
-  opened: boolean = false;
-
   constructor(private apiClient: ApiClientService) {
-    this.apiClient.getData('/todos').subscribe((data) => console.log(data));
+    this.apiClient.getData('/todos').subscribe((data) => console.log(data)); //just an example of call
   }
+
+  showInput: boolean = false
+
+  allFiles: File[] = [
+    {
+      name: 'ciao',
+      extension: 'c',
+    },
+  ];
+
+  get files() {
+    return this.allFiles;
+  }
+
+  addFile(name: string, extension: string) {
+    this.allFiles.unshift({
+      name,
+      extension,
+    });
+    this.showInput = false
+  }
+
+  toggleInput() {
+    this.showInput = !this.showInput;
+  }
+  
 }
