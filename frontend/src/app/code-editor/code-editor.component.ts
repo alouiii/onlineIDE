5eaefa13001da991ef5a73e5cf2170cccbd386ae
Mohamed Nureddin Ali Mansuri <mohamed.mansuri@tum.de>
2023-12-30 10:58:30 +0100

    Frontend end compile button construction + clear button

--- frontend/src/app/code-editor/code-editor.component.ts
+++ frontend/src/app/code-editor/code-editor.component.ts
@@ -4,6 +4,8 @@ import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
+import { CompileService } from '../compile.service';
 
 @Component({
   selector: 'app-code-editor',
@@ -25,6 +27,11 @@ export class CodeEditorComponent {
   };
   code: string = 'function x() {\nconsole.log("Hello world!");\n}';
 
+  constructor(
+    private httpClient: HttpClient,
+    private compileService: CompileService
+  ) {}
+
   changeLanguage(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     const selectedLanguage = selectElement.value;
@@ -32,5 +39,39 @@ export class CodeEditorComponent {
       theme: 'vs-light',
       language: selectedLanguage,
     };
+  }
+
+  save() {
+    localStorage.setItem('savedCode', this.code);
+  }
+
+  compile() {
+    this.compileService.compileCode(this.code).subscribe({
+      next: (response) => {
+        console.log('Compilation successful:', response);
+      },
+      error: (error) => {
+        console.error('Compilation error:', error);
+      },
+    });
+  }
+
+  run() {
+    const apiUrl = '/api/run'; // run API endpoint
+    const payload = {
+      language: this.editorOptions.language,
+      code: this.code,
+    };
+
+    //logs here will stay tell tested with api, once the connection is confirmed it will be removed
+    console.log(`Sending code to ${apiUrl} for execution...`, payload);
+    this.httpClient.post(apiUrl, payload).subscribe({
+      next: (response) => {
+        console.log('Execution successful:', response);
+      },
+      error: (error) => {
+        console.error('Execution error:', error);
+      },
+    });
   }
 }
---
+++ frontend/src/app/compile.service.ts
@@ -0,0 +1,37 @@
+// compile.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+export interface CompileApiResponse {
+  output: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CompileService {
+  private compileUrl = '/api/compiler';
+  private isMock = true; // Flag to control the mock behavior
+  private compileOutputSource = new BehaviorSubject<string>('');
+  currentOutput = this.compileOutputSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  compileCode(code: string): Observable<any> {
+    if (this.isMock) {
+      // Mock response
+      const mockOutput = `Mocked output for code: ${code}`;
+      this.compileOutputSource.next(mockOutput);
+      return of({ output: mockOutput });
+    } else {
+      // Actual HTTP request
+      return this.http.post<CompileApiResponse>(this.compileUrl, { code }).pipe(
+        tap((response) => {
+          this.compileOutputSource.next(response.output);
+        })
+      );
+    }
+  }
+}
