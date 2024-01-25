e9504680739b75efecca10d4bec4a34e6328b343
mousamax <mousa.abdelmaksoud@tum.de>
2024-01-25 19:56:42 +0100

    feat: connect onlineide services to Eureka server

--- dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/DarkmodeApplication.java
+++ dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/DarkmodeApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class DarkmodeApplication {
 	private boolean isDarkModeEnabled = false;
