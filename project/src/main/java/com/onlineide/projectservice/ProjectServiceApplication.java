61f853a0718c154c7cb7de0d6783eaeb233f0b2e
mousamax <mousa.abdelmaksoud@tum.de>
2024-01-25 21:35:19 +0100

    fix: remove @EnableEurekaClient deprecated

--- project/src/main/java/com/onlineide/projectservice/ProjectServiceApplication.java
+++ project/src/main/java/com/onlineide/projectservice/ProjectServiceApplication.java
@@ -2,10 +2,8 @@ package com.onlineide.projectservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class ProjectServiceApplication {
 
 	public static void main(String[] args) {
