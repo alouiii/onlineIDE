1fa22d29fed32534d4372e1b1045a9ab2b3fcbef
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-01-27 20:48:50 +0100

    moved the darkmode microservices into the outside

---
+++ darkmode/src/main/java/com/onlineide/darkmodeservice/DarkmodeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.onlineide.darkmodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkmodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkmodeServiceApplication.class, args);
+	}
+}
