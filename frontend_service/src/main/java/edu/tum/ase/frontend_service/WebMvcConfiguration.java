7c64caf598ac113608f6ee7769288a3290097367
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-01-27 16:35:28 +0100

    now routing works even when refreshing the page

--- frontend_service/src/main/java/edu/tum/ase/frontend_service/WebMvcConfiguration.java
+++ frontend_service/src/main/java/edu/tum/ase/frontend_service/WebMvcConfiguration.java
@@ -1,8 +1,13 @@
 package edu.tum.ase.frontend_service;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
@@ -10,6 +15,17 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                            Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
     }
+
 }
