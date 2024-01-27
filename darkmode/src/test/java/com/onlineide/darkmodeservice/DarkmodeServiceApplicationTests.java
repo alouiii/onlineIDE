7f956e9cae3fc6b9c9de73fb261c22bf25b06868
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-01-27 20:49:41 +0100

    test main file added (no tests yet)

---
+++ darkmode/src/test/java/com/onlineide/darkmodeservice/DarkmodeServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.onlineide.darkmodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkmodeServiceApplicationTests {
+
+    @Test
+	void contextLoads() {
+	}
+}
