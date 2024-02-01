a268382b76856f50faf37319b0776e98b3f1da1b
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-02-01 20:25:29 +0100

    frontend auth

--- frontend_service/frontend/src/app/components/home/home.component.ts
+++ frontend_service/frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,13 @@ import { Router } from '@angular/router';
   imports: [MatButtonModule],
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {
+    this.authService.authenticated.subscribe(res => {
+      if(res){
+        this.router.navigate(['/projects']);
+      }
+    });
+  }
 
   login() {
     console.log('login');
