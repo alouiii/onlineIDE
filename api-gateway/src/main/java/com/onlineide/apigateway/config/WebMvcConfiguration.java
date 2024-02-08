3d631ca9b815418fd25e68fe1b624ea4242ede8b
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-02-08 12:33:30 +0100

    added cors

--- api-gateway/src/main/java/com/onlineide/apigateway/config/WebMvcConfiguration.java
+++ api-gateway/src/main/java/com/onlineide/apigateway/config/WebMvcConfiguration.java
@@ -1,30 +1,19 @@
-// package com.onlineide.apigateway.config;
+package com.onlineide.apigateway.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.core.io.ClassPathResource;
-// import org.springframework.core.io.Resource;
-// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// import java.io.IOException;
 
-// @Configuration
-// public class WebMvcConfiguration implements WebMvcConfigurer {
-//     @Override
-//     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//         registry.addResourceHandler("/**", "/")
-//                 .setCachePeriod(1)
-//                 .addResourceLocations("classpath:/static/")
-//                 .resourceChain(true)
-//                 .addResolver(new PathResourceResolver() {
-//                     @Override
-//                     protected Resource getResource(String resourcePath, Resource location) throws IOException {
-//                         Resource requestedResource = location.createRelative(resourcePath);
-//                         return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-//                                 : new ClassPathResource("/static/index.html");
-//                     }
-//                 });
-//     }
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // replace this http://localhost:8010 with frontend service
+                .allowedOrigins("http://localhost:8010")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
 
-// }
+}
