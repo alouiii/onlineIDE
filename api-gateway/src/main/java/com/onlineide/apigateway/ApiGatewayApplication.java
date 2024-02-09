c3a0b961f1772cd5de9e92603d82230a092e7186
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-02-09 11:57:58 +0100

    added zuul to apigateway

--- api-gateway/src/main/java/com/onlineide/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/onlineide/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,8 @@ import java.security.Principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,8 @@ import jakarta.servlet.http.HttpServletRequest;
 @SpringBootApplication
 @RestController
 @CrossOrigin(origins = "http://localhost:8010", maxAge = 3600)
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
