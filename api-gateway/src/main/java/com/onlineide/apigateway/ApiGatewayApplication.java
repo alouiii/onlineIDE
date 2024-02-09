5dc5b928c657cfce9be96d0966994efe1e59372f
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-02-09 19:13:23 +0100

    removed zull from springmain

--- api-gateway/src/main/java/com/onlineide/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/onlineide/apigateway/ApiGatewayApplication.java
@@ -5,7 +5,6 @@ import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +20,6 @@ import jakarta.servlet.http.HttpServletRequest;
 @SpringBootApplication
 @RestController
 @CrossOrigin(origins = "http://localhost:8010", maxAge = 3600)
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
