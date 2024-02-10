1e37981a4b2c4c57e812eb43530a733402cca06c
Mohamed Mansuri <117668345+Mansuri98@users.noreply.github.com>
2024-02-10 15:58:43 +0100

    conflict issues

--- frontend_service/frontend/src/app/auth.guard.ts
+++ frontend_service/frontend/src/app/auth.guard.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { AuthService } from './auth.service';
 import { Observable, tap } from 'rxjs';
 
@@ -7,8 +13,7 @@ import { Observable, tap } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
   /**
    * Method which is activated before each route change to a route which requires this
@@ -16,18 +21,21 @@ export class AuthGuard implements CanActivate {
   * E.g. [{ path: 'secure', component: SecureComponent, canActivate: [AuthGuard]}, ...]
   *
   **/
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> { 
-    return this.authService.authenticated
-      // pipe each value that is emitted by the observable
-      .pipe(
-      // tap will intercept each emission of a value and perform some action tap(authenticated => {
-      tap(authenticated => {  
-        if (!authenticated) {
-          this.router.navigate(['/']); // Navigate to home page after logout
-        }
-      }));
-    }
-
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return (
+      this.authService.authenticated
+        // pipe each value that is emitted by the observable
+        .pipe(
+          // tap will intercept each emission of a value and perform some action tap(authenticated => {
+          tap((authenticated) => {
+            if (!authenticated) {
+              this.router.navigate(['/']); // Navigate to home page after logout
+            }
+          })
+        )
+    );
+  }
 }
-    
-    
