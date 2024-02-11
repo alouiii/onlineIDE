baf439ca5bdceda41e1f23056ca781bf0a64635f
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-02-11 16:16:34 +0100

    added cross origin in darkmode

--- dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/configuration/WebConfiguration.java
+++ dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/configuration/WebConfiguration.java
@@ -9,10 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    // @Override
-    // public void addCorsMappings(CorsRegistry registry) {
-    //     registry.addMapping("/**").allowedMethods("*");
-    // }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*", "x-csrf-token") // Allow 'x-csrf-token' header
+                .allowedOrigins("http://34.125.30.158:8084")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
 
     @Override
     public void configureAsyncSupport(final AsyncSupportConfigurer configurer) {
