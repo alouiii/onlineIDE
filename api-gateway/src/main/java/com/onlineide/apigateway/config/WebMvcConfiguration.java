1d980a156bf6c17d79b5b026cc794a11b689863e
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-02-11 21:30:31 +0100

    remove enable web mvc

--- api-gateway/src/main/java/com/onlineide/apigateway/config/WebMvcConfiguration.java
+++ api-gateway/src/main/java/com/onlineide/apigateway/config/WebMvcConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -15,7 +14,6 @@ import java.io.IOException;
 
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Value("${cors.origins}")
