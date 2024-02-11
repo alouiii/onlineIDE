25402d7e989ceb128f6a2da2d2c62219f476df07
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-02-11 13:26:09 +0100

    removed interceptors

--- api-gateway/src/main/java/com/onlineide/apigateway/config/WebMvcConfiguration.java
+++ api-gateway/src/main/java/com/onlineide/apigateway/config/WebMvcConfiguration.java
@@ -2,26 +2,16 @@ package com.onlineide.apigateway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-    private final CustomInterceptor customInterceptor;
 
     @Value("${cors.origins}")
     private String corsOrigins;
 
-    public WebMvcConfiguration(CustomInterceptor customInterceptor) {
-        this.customInterceptor = customInterceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(customInterceptor);
-    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         String[] origins = corsOrigins.split(",");
