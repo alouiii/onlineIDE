53fbb90666b13ed35c19a3bfbf38fbdf2d14daa7
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2024-02-11 15:11:50 +0100

    added darkmode functionality

---
+++ dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/configuration/WebConfiguration.java
@@ -0,0 +1,24 @@
+package edu.tum.ase.darkmode.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**").allowedMethods("*");
+    // }
+
+    @Override
+    public void configureAsyncSupport(final AsyncSupportConfigurer configurer) {
+        // Since Server-Sent Events (SSE) establish a long-lived connection, and if there is no activity (no data sent) for a certain period, 
+        // it may result in a timeout exception.
+        // So to prevent initiating a new connection everytime set no timeout
+        configurer.setDefaultTimeout(0); // Set timeout value in milliseconds (0 means no timeout)
+    }
+}
---
+++ dark-mode/darkmode/src/main/java/edu/tum/ase/darkmode/controller/DarkmodeController.java
@@ -0,0 +1,29 @@
+package edu.tum.ase.darkmode.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.tum.ase.darkmode.service.DarkmodeService;
+
+
+@RestController
+@RequestMapping("/api/dark-mode")
+public class DarkmodeController {
+
+    @Autowired
+    private DarkmodeService darkmodeService;
+
+    @RequestMapping(path = "/toggle", method = RequestMethod.GET)
+	public void toggleDarkMode() {
+		darkmodeService.toggleDarkMode();
+	}
+
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	public boolean getDarkModeStatus(){
+		return darkmodeService.getDarkModeStatus();
+	}
+
+}
